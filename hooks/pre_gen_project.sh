d44b17e2a8693767e851fc81b45a11f88e0ee00c
Tekktrik <tekktrik@gmail.com>
2023-06-15 00:03:31 -0400

    Check for programs in pre-gen hook

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+check_for_program () {
+    commandpath="$(command -v $1)"
+    if [ -z $commandpath ]; then
+        echo "$1 not installed!  Exiting PCB cookiecutter."
+        exit 1
+    fi
+}
+
+# Check for `git`
+check_for_program git
+
+# Check for `python3`
+check_for_program python3
+
+# Check for `wget`
+check_for_program wget
+
 echo "pre-generate"
 echo "{{cookiecutter.pid}} is PID"
 exit 
